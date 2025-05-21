ec20a416c620acf4681aa7863778fdc9466810fe
Cham <450139220@qq.com>
2025-05-21 23:46:49 +0800

    feat(autopairs): autopair <> except js and ts

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -3,7 +3,15 @@ return {
   config = function()
     require("autoclose").setup({
       keys = {
-        ["<"] = { escape = true, close = true, pair = "<>", disabled_filetypes = {} },
+        ["<"] = {
+          escape = true,
+          close = true,
+          pair = "<>",
+          disabled_filetypes = {
+            "javascript",
+            "typescript",
+          }
+        }
       }
     })
   end
